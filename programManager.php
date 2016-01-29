ca6d8e1c194e1737a637db12f927a98c56e66573
kitdoor <kitdoor@yahoo.com>
2016-01-28 22:22:26 -0500

    1/29

--- programManager.php
+++ programManager.php
@@ -7,60 +7,60 @@ class ProgramManager
 	const DBNAME = "mannitechcorp";
 	function openConn(){
 		$this->db = new Database(self::SERVERNAME, self::USERNAME, self::PASSWORD, self::DBNAME);
-		$this->con = $this->db->db_connect();
+		$this->db_con = $this->db->db_connect();
 	}
 	function createPerson($tempPersonDTO) {
 		try{
 			self::openConn();
-			$this->con->beginTransaction();
-			$person_id = PersonDAO::createPerson($this->con, $tempPersonDTO);
+			$this->db_con->beginTransaction();
+			$person_id = PersonDAO::createPerson($this->db_con, $tempPersonDTO);
 			//CHECK HERE IF DTOs ARE EMPTY???
 			$phoneDTO = $tempPersonDTO->getPhoneDTO();
 			$phoneDTO->setPersonID($person_id);
 			$addressDTO = $tempPersonDTO->getAddrDTO();
 			$addressDTO->setPersonID($person_id);
-			$phone_id = PhoneDAO::createPhone($this->con, $phoneDTO);
-			$address_id = AddressDAO::createAddress($this->con, $addressDTO);
+			$phone_id = PhoneDAO::createPhone($this->db_con, $phoneDTO);
+			$address_id = AddressDAO::createAddress($this->db_con, $addressDTO);
 			$return = new PersonsDTO($person_id, "CREATED");
-			$this->con->commit();
+			$this->db_con->commit();
 			self::closeConn();
 			return $return;
 		}
 		catch(PDOException $e)
 		{
-			$this->con->rollBack();
+			$this->db_con->rollBack();
 		}
 	}
-	function readPerson(){
+	function loadAllPersons(){
 	    self::openConn();
-		$personList = PersonDAO::readPersonList($this->con);
+		$personList = PersonDAO::readPersonList($this->db_con);
 		$personListDTO = new PersonsDTO($personList, "READ");
 		self::closeConn();
 		return $personListDTO;
 	}
 	function deletePerson($person_id) {
 		self::openConn();
-		$this->con->beginTransaction();
+		$this->db_con->beginTransaction();
 		try 
 		{
-			PhoneDAO::deletePhone($this->con, $person_id);
-			AddressDAO::deleteAddress($this->con, $person_id);
-			$result = PersonDAO::deletePerson($this->con, $person_id);
+			PhoneDAO::deletePhone($this->db_con, $person_id);
+			AddressDAO::deleteAddress($this->db_con, $person_id);
+			$result = PersonDAO::deletePerson($this->db_con, $person_id);
 			$return = new ResultDTO($result, "DELETED");
 			$json = json_encode($return);
 			echo $json;
-			$this->con->commit();
+			$this->db_con->commit();
 			self::closeConn();
 		}
 		catch(PDOException $e)
 		{
 			$sql_phone = "<br>" . $e->getMessage();
-			$this->con->rollBack();
+			$this->db_con->rollBack();
 		}
 	}
 	function updatePerson($personID){
 		self::openConn();
-		$result = PersonDAO::getPerson($this->con, $personID);
+		$result = PersonDAO::getPerson($this->db_con, $personID);
 		$return = new ResultDTO($result, "DISPLAYED");
 		$json = json_encode($return);
 		echo $json;
@@ -68,27 +68,27 @@ class ProgramManager
 	}
 	function updatePersonRecord($personDTO){
 		self::openConn();
-		$this->con->beginTransaction();
+		$this->db_con->beginTransaction();
 		try {
-			AddressDAO::updateAddress($this->con, $personDTO->getAddrDTO());
-			PhoneDAO::updatePhone($this->con, $personDTO->getPhoneDTO());
-			$person_id = PersonDAO::updatePerson($this->con, $personDTO);
+			AddressDAO::updateAddress($this->db_con, $personDTO->getAddrDTO());
+			PhoneDAO::updatePhone($this->db_con, $personDTO->getPhoneDTO());
+			$person_id = PersonDAO::updatePerson($this->db_con, $personDTO);
 			$return = new ResultDTO($person_id, "UPDATED");
 			$json = json_encode($return);
 			echo $json;
-			$this->con->commit();
+			$this->db_con->commit();
 			self::closeConn();
 		}
 		catch(PDOException $e)
 		{
 			$person_id = "person insert Error: " . $e->getMessage();
-			$this->con->rollBack();
+			$this->db_con->rollBack();
 		}
 	}/*
 	function getPhoneTypes(){
 		self::openConn();
 		$this->phoneDAO = new PhoneDAO();
-		$phoneTypeList = $this->phoneDAO->readPhoneTypeList($this->con);
+		$phoneTypeList = $this->phoneDAO->readPhoneTypeList($this->db_con);
 		$json = json_encode($phoneTypeList);
 		echo $json;
 		self::closeConn();
@@ -97,7 +97,7 @@ class ProgramManager
 	function createAddr($tempAddressDTO) {
 		$this->addr_dto = $tempAddressDTO;
 		$this->addrDAO = new AddressDAO();
-		$result = $this->addrDAO->createAddress($this->con, $tempAddressDTO);
+		$result = $this->addrDAO->createAddress($this->db_con, $tempAddressDTO);
 		$return = new ResultDTO($result, "CREATED");
 		$json = json_encode($return);
 		echo $json;
@@ -105,40 +105,42 @@ class ProgramManager
 	
 	function deleteAddr($address_id) {
 		$this->addrDAO = new AddressDAO();
-		$result = $this->addrDAO->deleteAddress($this->con, $address_id);
+		$result = $this->addrDAO->deleteAddress($this->db_con, $address_id);
 		$return = new ResultDTO($result, "CREATED");
 		$json = json_encode($return);
 		echo $json;
 	}
 	function readAddr(){
 		$this->addrDAO = new AddressDAO();
-		$result = $this->addrDAO->readAddressList($this->con);
+		$result = $this->addrDAO->readAddressList($this->db_con);
 		$return = new ResultDTO($result, "CREATED");
 		$json = json_encode($return);
 		echo $json;
 	}
 	function updateAddr($addrID){
 		$this->addrDAO = new AddressDAO();
-		$result = $this->addrDAO->getAddress($this->con, $addrID);
+		$result = $this->addrDAO->getAddress($this->db_con, $addrID);
 		$return = new ResultDTO($result, "CREATED");
 		$json = json_encode($return);
 		echo $json;
 	}
 	function updateAddrRecord($addrDTO){
 		$this->addrDAO = new AddressDAO();
-		$result = $this->addrDAO->updateAddress($this->con, $addrDTO);
+		$result = $this->addrDAO->updateAddress($this->db_con, $addrDTO);
 		$return = new ResultDTO($result, "CREATED");
 		$json = json_encode($return);
 		echo $json;
 	}
-	function getState(){
-		$stateList = StateDAO::readStateList($this->con);
-		$json = json_encode($stateList);
-		echo $json;
-	}
 	*/
+	function getState(){
+	    self::openConn();
+		$stateList = StateDAO::readStateList($this->db_con);
+		self::closeConn();
+		return $stateList;
+	}
+
 	function closeConn() {
-		$this->db->db_close($this->con);
+		$this->db->db_close($this->db_con);
 	}
 
 }
